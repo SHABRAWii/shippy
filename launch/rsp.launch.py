be2486e6dc5733678e282bec04b8bf8d55dcc161
Yousef Mohamed Mohamed ElShabrawii <youssef.elshabrawii@gmail.com>
2024-04-15 06:56:25 +0200

    Edit rsp to load from gazebo instead of main description

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('shippy'))
-    xacro_file = os.path.join(pkg_path,'description','shippy.urdf.xacro')
+    xacro_file = os.path.join(pkg_path,'description', 'gazebo', 'shippy.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
